d0497704795543346c08e549336e0a1ce4a8ebbc
Alec Ritson <alecr@builtwithmomentum.com>
2016-04-22 11:50:45 +0100

    Enabled product search

--- src/Product.php
+++ src/Product.php
@@ -7,4 +7,9 @@ class Product extends Client
 	{
 		return new Response($this->request('products'));
 	}
+
+	public function search($options = array())
+	{
+		return new Response($this->request('products', array('query' => $options)));
+	}
 }
